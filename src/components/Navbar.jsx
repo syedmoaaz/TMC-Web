c1c4a1eeaaaaba910bc7bbfd621fabee9d7b4818
Syed Moaaz Kamal <workmoaaz@gmail.com>
2026-07-05 22:54:39 +0500

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "../assets/logo.png"; // <-- apna logo yahan rakho
+import logo from "../assets/logo.png"; 
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
